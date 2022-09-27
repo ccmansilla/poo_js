ccd5d01177197f6d1ff46c82c3bda7356acbe2f9
ccmansilla <carloscmansilla@gmail.com>
2022-09-27 07:52:18 -0300

    ejemplo de poo en javacript

---
+++ script.js
@@ -0,0 +1,15 @@
+class Animal { 
+    constructor(nombre, propietario, raza, edad, actividad, comida, visita, vacunas) {
+        this.nombre = nombre;
+        this.propietario = propietario;
+        this.raza = raza;
+        this.edad = edad;
+        this.actividad = actividad;
+        this.comida = comida;
+        this.visita = visita;
+        this.vacunas = vacunas;
+    }
+}
+
+let gato = new Animal('Codito', 'Susi', 'Gato', 2, 'dormir', 'Gati', 6, 2);
+console.log(gato);
