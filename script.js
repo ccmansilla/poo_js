c166e3c0d1a554cd00f9e3fea7648e66016b9873
ccmansilla <carloscmansilla@gmail.com>
2022-09-27 13:04:20 -0300

    agrego document.write

--- script.js
+++ script.js
@@ -27,3 +27,12 @@ function Animal(nombre, propietario, raza, edad, actividad, comida, visita, vacu
 let gato = new Animal('Codito', 'Susi', 'Gato', 2, 'dormir', 'Gati', 6, 2);
 console.log(gato);
 
+document.write('nombre = ' + gato.nombre + '<br>');
+document.write('propietario = ' + gato.propietario + '<br>');
+document.write('raza = ' + gato.raza + '<br>');
+document.write('edad = ' + gato.edad + '<br>');
+document.write('actividad = ' + gato.actividad + '<br>');
+document.write('comida = ' + gato.comida + '<br>');
+document.write('visita = ' + gato.visita + '<br>');
+document.write('vacunas = ' + gato.vacunas + '<br>');
+
