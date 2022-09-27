93c43ef84d81c2ab3b4ce270464e1a762af481dd
ccmansilla <carloscmansilla@gmail.com>
2022-09-27 08:51:39 -0300

    constructor con funcion

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 class Animal { 
     constructor(nombre, propietario, raza, edad, actividad, comida, visita, vacunas) {
         this.nombre = nombre;
@@ -10,6 +11,19 @@ class Animal {
         this.vacunas = vacunas;
     }
 }
+*/
+
+function Animal(nombre, propietario, raza, edad, actividad, comida, visita, vacunas) {
+    this.nombre = nombre;
+    this.propietario = propietario;
+    this.raza = raza;
+    this.edad = edad;
+    this.actividad = actividad;
+    this.comida = comida;
+    this.visita = visita;
+    this.vacunas = vacunas;
+}
 
 let gato = new Animal('Codito', 'Susi', 'Gato', 2, 'dormir', 'Gati', 6, 2);
 console.log(gato);
+
